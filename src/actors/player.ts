d7f21c5eecf29fab3259daacf3a9ccdc6b8aa14e
Krishan Wyse <kwysek@gmail.com>
2021-11-15 21:47:23 +0000

    Add basic physics

--- src/actors/player.ts
+++ src/actors/player.ts
@@ -1,14 +1,37 @@
-import { Actor, Engine } from "excalibur";
+import { Actor, CollisionType, Engine, Input, Vector, vec } from "excalibur";
 import { SpriteSheets } from "../resources";
 
 export default class Player extends Actor {
   constructor() {
-    super({ x: 64, y: 64, width: 64, height: 32 });
+    super({
+      x: 64,
+      y: 64,
+      width: 64,
+      height: 32,
+      collisionType: CollisionType.Active,
+    });
   }
 
   public onInitialize(_engine: Engine) {
     this.graphics.use(SpriteSheets.player_idle.sprites[0]);
   }
 
-  public onPostUpdate(_engine: Engine, _delta: number) {}
+  public onPostUpdate(engine: Engine, _delta: number) {
+    if (engine.input.keyboard.isHeld(Input.Keys.Right)) {
+      this.move(vec(1, 0));
+    }
+
+    if (engine.input.keyboard.isHeld(Input.Keys.Left)) {
+      this.move(vec(-1, 0));
+    }
+
+    if (engine.input.keyboard.wasPressed(Input.Keys.Space)) {
+      this.vel.y -= 100;
+    }
+  }
+
+  private move(vec: Vector) {
+    this.pos.x += vec.x;
+    this.pos.y += vec.y;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Engine } from "excalibur";
+import { Actor, CollisionType, Color, Engine, Physics, vec } from "excalibur";
 import Player from "./actors/player";
 import { loader } from "./resources";
+
+Physics.acc = vec(0, 500);
 
 const game = new Engine({
   width: 800,
@@ -10,4 +12,13 @@ const game = new Engine({
 game.start(loader);
 
 const player = new Player();
+const ground = new Actor({
+  pos: vec(game.halfDrawWidth, game.drawHeight),
+  width: game.drawHeight,
+  height: 100,
+  color: Color.Green,
+  collisionType: CollisionType.Fixed,
+});
+
+game.add(ground);
 game.add(player);
